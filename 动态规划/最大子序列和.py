0aa464661597cfa6abd364b45d5673ad40a8f2ca
noobexplore <449053131@qq.com>
2021-11-12 11:17:22 +0800

    update

--- 动态规划/最大子序列和.py
+++ 动态规划/最大子序列和.py
@@ -17,13 +17,20 @@ class Solution:
         '''
         动态规划，解题思路：
         1. 理解题意，首先该题解决的是连续的子序列，重点是连续，不然只找最大的相加就OK
-        2. 定义子问题，dp[i]
+        2. 定义子问题，dp[i]为以第i个数字结尾的子序列的和
+        3. 定义转移公式，dp[i] = max(0, dp[i-1]) + nums[i]
+        4. 确定初始条件，dp[0] = nums[0]
+        5. 确定输出结果，由于dp是对应的所有i结果，所以要取dp最大的值
         '''
-        
+        dp = [0 for _ in range(len(nums))]
+        dp[0] = nums[0]
+        for i in range(1, len(nums)):
+            dp[i] = max(dp[i-1], 0) + nums[i]
+        return max(dp)
 
 
 if __name__ == '__main__':
     solution = Solution()
-    n = [-2, -1]
-    maxsum = solution.maxSubArray_greedy(n)
+    n = [-2,1,-3,4,-1,2,1,-5,4]
+    maxsum = solution.maxSubArray_dynamic(n)
     pass
