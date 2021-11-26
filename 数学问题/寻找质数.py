12268e4767e1512eb44fe2b5bf59815e594e6b5f
noobexplore <449053131@qq.com>
2021-11-26 14:32:40 +0800

    find Primes

---
+++ 数学问题/寻找质数.py
@@ -0,0 +1,23 @@
+import math
+
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        """埃筛法"""
+        count = 0
+        # 先初始化n的数组全部值为True
+        isPrimes = [True for _ in range(n)]
+        sq = int(math.sqrt(n))
+        for i in range(2, sq):
+            for j in range(i**2, n, i):
+                if isPrimes[i]:
+                    isPrimes[j] = False
+        # 然后最终计数数组中为true的个数
+        for p in range(2, n):
+            count += isPrimes[p]
+        return count
+
+if __name__ == '__main__':
+    n = 20
+    solution = Solution()
+    cp = solution.countPrimes(n)
+    pass
